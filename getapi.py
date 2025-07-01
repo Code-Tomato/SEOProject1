6a9047176da331fd716a32ff2c4a9138e0203d74
MarcoGuzman-Balcazar <maguzm2@emory.edu>
2025-07-01 17:18:21 -0400

    Use Cheapshark and gemini to print formatted game deals

--- getapi.py
+++ getapi.py
@@ -1,15 +1,28 @@
 import os 
 from google import genai
 from google.genai import types
+import requests
+import json
 
 # Set environment variables
 my_api_key = os.getenv('GENAI_API_KEY')
 
 genai.api_key = my_api_key
 
-# WRITE YOUR CODE HER
+url="https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
+data = requests.get(url).json()
+data_str = ""
+for game in data:
+    print(game)
+    data_str += json.dumps(game, indent=2) + "\n"
 
-# Create an genAI client using the key from our environment variable
+
+# for game in data:
+#     print(f"Title: {game['title']}, Sales Price: {game['salePrice']}, Normal Price: {game['normalPrice']}")
+    
+user_input = input("What type of games and price range are you looking for? ")
+
+#Create an genAI client using the key from our environment variable
 client = genai.Client(
     api_key=my_api_key,
 )
@@ -18,9 +31,11 @@ client = genai.Client(
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     config=types.GenerateContentConfig(
-      system_instruction="You are a university instructor and can explain programming concepts clearly in a few words."
+      system_instruction="Parse through the data and let me know "
     ),
-    contents="What are the advantages of pair programming?",
+    contents=[user_input, data_str]
 )
 
 print(response.text)
+
+
